5f07b7314c2c93f1b4f9bb9d634ac5e87747ab55
Jens Kristoffer Retian Markussen <jens.markussen@knowit.no>
2019-05-27 21:54:01 +0200

    proposed solution targets

---
+++ Assets/Behaviours/ColorUpdater.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ColorUpdater : MonoBehaviour
+{
+    private IList<Material> _childMaterials;
+
+    void Start()
+    {
+        _childMaterials = GetComponentsInChildren<MeshRenderer>()
+            .Select(x => x.material).ToList();
+    }
+
+    public void SetColor(Color color)
+    {
+        foreach (var childMaterial in _childMaterials)
+        {
+            childMaterial.color = color;
+        }
+    }
+}
---
+++ Assets/Behaviours/TargetHitDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ColorUpdater), typeof(BoxCollider))]
+public class TargetHitDetector : MonoBehaviour
+{
+    public Color HitColor = Color.red;
+
+    private ColorUpdater _colorUpdater;
+
+    void Start()
+    {
+        _colorUpdater = GetComponent<ColorUpdater>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        _colorUpdater.SetColor(HitColor);
+    }
+}
