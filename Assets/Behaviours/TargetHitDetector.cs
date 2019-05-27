670bf2b449c0d830d2d25ece94b1b9094f157c13
Jens Kristoffer Retian Markussen <jens.markussen@knowit.no>
2019-05-27 22:28:07 +0200

    proposed solution gui and gamestate

--- Assets/Behaviours/TargetHitDetector.cs
+++ Assets/Behaviours/TargetHitDetector.cs
@@ -8,14 +8,23 @@ public class TargetHitDetector : MonoBehaviour
     public Color HitColor = Color.red;
 
     private ColorUpdater _colorUpdater;
+    private GameManager _gameManager;
+
+    private bool _isHit = false;
 
     void Start()
     {
         _colorUpdater = GetComponent<ColorUpdater>();
+        _gameManager = FindObjectOfType<GameManager>();
     }
 
     void OnTriggerEnter(Collider other)
     {
-        _colorUpdater.SetColor(HitColor);
+        if (!_isHit)
+        {
+            _colorUpdater.SetColor(HitColor);
+            _gameManager.AddScore();
+            _isHit = true;
+        }
     }
 }
---
+++ Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject ScoreText;
+    public GameObject WinnerText;
+
+    public int MaxScore = 3;
+
+    private int _score = 0;
+
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    public void AddScore()
+    {
+        _score += 1;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        ScoreText.GetComponent<Text>().text = $"Score: {_score}";
+        WinnerText.SetActive(_score == MaxScore);
+    }
+}
